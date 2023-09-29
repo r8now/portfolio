f0626776f8e347951d79cf35120510bd88d9beb5
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-09-29 20:56:56 +0200

    links imported

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-
+import {links} from "../data"
 
 const Navbar = () => {
   return (
     <nav className="bg-emerald-100">
-        <div className="mx-auto max-w-7xl px-8 py-4 flex"></div>
+        <div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:item-center sm:py-8">
+          <h2 className="">Web<span className="text-emerald-600">Dev</span></h2>
+          <div className="flex gap-x3">{links}</div>
+        </div>
     </nav>
   )
 }
