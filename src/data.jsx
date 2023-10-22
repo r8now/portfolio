c93043dcee50ede4707a3a86578574af9485b969
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-10-22 15:51:42 +0200

    updating information

--- src/data.jsx
+++ src/data.jsx
@@ -8,6 +8,7 @@ import {
   FaGithub,
   
 } from "react-icons/fa";
+import { SiCsharp, SiDotnet, SiMysql, SiTailwindcss } from "react-icons/si";
 
 
 export const links = [
@@ -22,43 +23,62 @@ export const skills = [
     id: nanoid(),
     title: "HTML&CSS",
     icon: <FaHtml5 className="h-16 w-16 text-emerald-500" />,
-    text: "Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.",
+    text: "Highly skilled in HTML & junior CSS skills, creating visually appealing and responsive websites for optimal user experiences.",
   },
   {
     id: nanoid(),
     title: "Javascript",
     icon: <FaJs className="h-16 w-16 text-emerald-500" />,
-    text: "Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality",
+    text: "Comfortable in JavaScript, building interactive and dynamic web applications with a focus on user interactions and functionality",
   },
   {
     id: nanoid(),
     title: "React",
     icon: <FaReact className="h-16 w-16 text-emerald-500" />,
-    text: "Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
+    text: "Know basics in React, developing front-end applications with a strong emphasis on component-based architecture.",
   },
-  {
-    id: nanoid(),
-    title: "Tailwind",
-    icon: <FaReact className="h-16 w-16 text-emerald-500" />,
-    text: "Beginner in Tailwind, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
-  },
+
   {
     id: nanoid(),
     title: "Wordpress",
     icon: <FaWordpress className="h-16 w-16 text-emerald-500" />,
-    text: "Beginner in Tailwind, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
+    text: "Comfortable working with wordpress CMS, adding custom CSS, but have also built a custom template from scratch with PHP",
   },
   {
     id: nanoid(),
     title: "PHP",
     icon: <FaPhp className="h-16 w-16 text-emerald-500" />,
-    text: "Beginner in Tailwind, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
+    text: "Have experience with PHP after building custom template for wordpress theme and also in creating REST-API",
   },
   {
     id: nanoid(),
     title: "Github",
     icon: <FaGithub className="h-16 w-16 text-emerald-500" />,
-    text: "Beginner in Tailwind, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
+    text: "Beginner in Github, know basics and able to save my work and creating branches and learning real world work scenarios",
+  },
+  {
+    id: nanoid(),
+    title: "C#",
+    icon: <SiCsharp className="h-16 w-16 text-emerald-500" />,
+    text: "Know basics in C# ",
+  },
+  {
+    id: nanoid(),
+    title: ".Net",
+    icon: <SiDotnet className="h-16 w-16 text-emerald-500" />,
+    text: "Know basics in .NET finished course in .NET in school by building pages with razor pages and microsoft SQL. ",
+  },
+  {
+    id: nanoid(),
+    title: "MySQL",
+    icon: <SiMysql className="h-16 w-16 text-emerald-500" />,
+    text: "Experience with MySQL when building custom templates for wordpress, but also creating database for small projects.",
+  },
+  {
+    id: nanoid(),
+    title: "Tailwind",
+    icon: <SiTailwindcss className="h-16 w-16 text-emerald-500" />,
+    text: "Beginner in Tailwind, learning to be efficient in using Tailwind.",
   },
 ];
 
@@ -87,4 +107,5 @@ export const projects = [
     title: 'third project',
     text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.',
   },
+  
 ];
