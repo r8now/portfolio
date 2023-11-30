b96cd1773d38bcc3bd3d389bd180444a4a780d0a
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2023-11-30 15:37:44 +0100

    Remove unnecessary CSS class in ProjectsCard component

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -10,7 +10,7 @@ const ProjectsCard = ({url,img,github,title,text}) => {
        
         className="w-full object-fit   rounded-t-lg h-64"
       />
-      <div className="capitalize p-8">
+      <div className="p-8">
         <h2>{title}</h2>
         <p className="mt-4 text-slate-700 leading-loose">{text}</p>
         <div className="mt-4 flex gap-x-4">
