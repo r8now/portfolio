1b41402d496da603be2914eff95defa913243eb8
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2024-01-11 22:07:58 +0100

    Add GitHub link to fourth project

--- src/data.jsx
+++ src/data.jsx
@@ -112,7 +112,7 @@ export const projects = [
     id: nanoid(),
     img: "/images/bygget.png",
     url: "https://brilliant-kringle-064f51.netlify.app/",
-   
+    github: "#/private repo",
     title: "Fourth project",
     text: "This was built with Vite, Tailwind css and React, i made a copy of a site and added pages to it and made it responsive for mobiles.",
   },
